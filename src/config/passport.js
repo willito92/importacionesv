305e165f4af512d89a6248fa1d70de77399e6201
unknown <george921981@gmail.com>
2020-05-21 13:01:35 -0500

    archivo passport y prueba

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LocalStrategy = require('passport-Local').Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/User');
 
